d971c10b46fbbde477f0d3a654b3d16c74c2ce09
Mastype <mzh1227@gmail.com>
2017-05-03 21:00:53 +0800

    BestTimeToBuyAndSold

---
+++ 123.股票买卖/better.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+//使用2维数据存储的DP解法。空间超
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        if(len < 2)
+        return 0;
+	    int stats[2][4] = {-prices[0], 0, INT_MIN, 0};
+	    for(int i = 1; i < len; i++) {
+	        stats[i%2][0] = max(stats[(i-1)%2][0], -prices[i]);
+	        stats[i%2][1] = max(stats[(i-1)%2][1], stats[(i-1)%2][0] + prices[i]);
+	        stats[i%2][2] = max(stats[(i-1)%2][2], stats[(i-1)%2][1] - prices[i]);
+	        stats[i%2][3] = max(stats[(i-1)%2][3], stats[(i-1)%2][2] + prices[i]);
+	    }
+	    return max(stats[(len-1)%2][1], stats[(len-1)%2][3]);
+    }
+};
+int main()
+{
+	vector<int> arr({ 5,4,3,2,1 });
+	cout << Solution().maxProfit(arr) << endl;
+}
---
+++ 123.股票买卖/code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+//使用2维数据存储的DP解法。空间超
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+        int size = prices.size();
+        if(size < 2)
+        return 0;
+		vector< vector<int> > arr(size, vector<int>(size, 0));
+		for (int i = 0; i < size; i++)
+			arr[i][i] = prices[i];
+
+		for (int i = 0; i < size; i++)
+		for (int j = i + 1; j < size; j++)  {
+			arr[i][j] = (arr[i][j - 1] < prices[j]) ? arr[i][j - 1] : prices[j];
+			int r = size - 1 - i;
+			int c = size - 1 - j;
+			if (r == c + 1)
+				arr[r][c] = (prices[j] - arr[i][j] > 0) ? prices[j] - arr[i][j] : 0;
+			else
+				arr[r][c] = (arr[r][c + 1] > prices[j] - arr[i][j]) ? arr[r][c + 1] : prices[j] - arr[i][j];
+		}
+		int res = (arr[size-1][0] > 0) ? arr[size-1][0] : 0;
+		for (int i = 1; i < size-1; i++){
+			int profit = arr[i][0] + arr[size - 1][i];
+			res = res > profit ? res : profit;
+		}
+		return res;
+    }
+};
+int main()
+{
+	vector<int> arr({ 5,4,3,2,1 });
+	cout << Solution().maxProfit(arr) << endl;
+}
